faaee16ccdd852c3b2e1422e6ac786e41306a926
cirocc <cairo.henrique.moraes@gmail.com>
2018-05-24 17:43:25 -0300

    Novo componente home

--- projeto-angular/src/app/app.module.ts
+++ projeto-angular/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FormularioComponent } from './formulario/formulario.component';
 import { ConsultarCepComponent } from './consultar-cep/consultar-cep.component';
 import { FormComponent } from './formulario/form/form.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormularioComponent,
     ConsultarCepComponent,
-    FormComponent
+    FormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
